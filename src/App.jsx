7b011e9c97e332f76a91bcaf8236bab713348c1a
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-03 16:46:31 +0000

    create RenderScores component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import allCountryScores from "../data/scores.jsx";
 
+function RenderScores(country) {
+  return country.scores.map((score) => (
+    <li key={crypto.randomUUID()}>
+      {score.n} {score.s}
+    </li>
+  ));
+}
+
+
 
 function App() {
   const countriesList = allCountryScores.map((country) => {
-    const scores = country.scores.map((score) => (
-      <li>
-        {score.n} {score.s}
-      </li>
-    ));
-
-
     return (
       <>
         <h2>High Scrores: {country.name}</h2>
         <p key={crypto.randomUUID()}>{country.name}</p>
-        <ul>{scores}</ul>
+        <ul>{RenderScores(country)}</ul>
       </>
     );
   });
