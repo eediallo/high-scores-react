21654e75d3d3c08c14e077302003613945a0c6e9
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-03 19:03:21 +0000

    moves component into components folder

---
+++ components/renderCountriesScores.jsx
@@ -0,0 +1,34 @@
+
+function sortScores(scores) {
+  return scores.sort((a, b) => b.s - a.s);
+}
+
+function sortCountryScoresByName(countries) {
+  return countries.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function RenderScores(country) {
+  const sortedScores = sortScores(country.scores);
+  return sortedScores.map((score) => (
+    <li className="score" key={crypto.randomUUID()}>
+      {" "}
+      {score.n} <span className="high-score">{score.s}</span>
+    </li>
+  ));
+}
+
+function RenderScoresPerCountry(allCountryScores) {
+  const sortedCountriesByName = sortCountryScoresByName(allCountryScores);
+  return sortedCountriesByName.map((country) => {
+    return (
+      <>
+        <div className="scores-container">
+          <h2>High Scrores: {country.name}</h2>
+          <ul>{RenderScores(country)}</ul>
+        </div>
+      </>
+    );
+  });
+}
+
+export {RenderScoresPerCountry}
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,6 @@
 import allCountryScores from "../data/scores.jsx";
+import { RenderScoresPerCountry } from "../components/renderCountriesScores.jsx";
 import "./App.css";
-
-function sortScores(scores) {
-  return scores.sort((a, b) => b.s - a.s);
-}
-
-function sortCountryScoresByName(countries) {
-  return countries.sort((a, b) => a.name.localeCompare(b.name));
-}
-
-function RenderScores(country) {
-  const sortedScores = sortScores(country.scores);
-  return sortedScores.map((score) => (
-    <li className="score" key={crypto.randomUUID()}>
-      {" "}
-      {score.n} <span className="high-score">{score.s}</span>
-    </li>
-  ));
-}
-
-function RenderScoresPerCountry(allCountryScores) {
-  const sortedCountriesByName = sortCountryScoresByName(allCountryScores);
-  return sortedCountriesByName.map((country) => {
-    return (
-      <>
-        <div className="scores-container">
-          <h2>High Scrores: {country.name}</h2>
-          <ul>{RenderScores(country)}</ul>
-        </div>
-      </>
-    );
-  });
-}
 
 function App() {
   return (
