43e57515d76aa4db1de14bbf4453f85231d7754c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-03 16:36:34 +0000

    render scores

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import allCountryScores from "../data/scores.jsx";
 
+
 function App() {
   const countriesList = allCountryScores.map((country) => {
+    const scores = country.scores.map((score) => (
+      <li>
+        {score.n} {score.s}
+      </li>
+    ));
+
+
     return (
       <>
         <h2>High Scrores: {country.name}</h2>
-        <p key={crypto.randomUUID()}>{country.name}</p>;
+        <p key={crypto.randomUUID()}>{country.name}</p>
+        <ul>{scores}</ul>
       </>
     );
   });
