d444ba4136ee716d3802edb01198c98c1af2a3c4
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-03 15:48:04 +0000

    render countries name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App(){
-  return 'Hi'
+import allCountryScores from "../data/scores.jsx";
+
+function App() {
+  const countriesList = allCountryScores.map((country) => {
+    return <li key={crypto.randomUUID()}>{country.name}</li>;
+  });
+
+  return (
+    <>
+      <h1>High Scores per country</h1>
+      {countriesList}
+    </>
+  );
 }
 
-export default App
+export default App;
