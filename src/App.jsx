ce5f80bb8136399b2520dd64072f3f318d4d47f1
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-09 14:42:08 +0000

    change to arrow functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,67 +2,27 @@ import allCountryScores from "../data/scores.jsx";
 import { HighScoreTable } from "../components/renderCountriesScores.jsx";
 import "./App.css";
 import { useState } from "react";
-
-function sortCountryScoresByName(countries) {
-  return countries.sort((a, b) => a.name.localeCompare(b.name));
-}
-
-function sortScores(scores, ascending = true) {
-  return scores.sort((a, b) => (ascending ? b.s - a.s : a.s - b.s));
-}
+import {
+  SortButton,
+  SortCountryScoresByName,
+  SortScores,
+} from "../components/sortButton.jsx";
 
 function App() {
-  const [btnTextContent, setBtnTextCont] = useState("Sort Scores Descending");
-  // ensure ascending sort by default
   const [sortedScores, setSortedScores] = useState(getSortedCountryScores());
-
-  function getSortedCountryScores() {
-    return sortCountryScoresByName(
+  const getSortedCountryScores = () => {
+    return SortCountryScoresByName(
       allCountryScores.map((country) => ({
         ...country,
-        scores: sortScores(country.scores),
+        scores: SortScores(country.scores),
       }))
     );
-  }
-
-  function sortScoresAscending() {
-    setSortedScores(
-      sortCountryScoresByName(
-        allCountryScores.map((country) => ({
-          ...country,
-          scores: [...sortScores(country.scores, false)],
-        }))
-      )
-    );
-  }
-
-  function sortScoresDescending() {
-    setBtnTextCont("Sort Scores Descending");
-    setSortedScores(
-      sortCountryScoresByName(
-        allCountryScores.map((country) => ({
-          ...country,
-          scores: [...sortScores(country.scores)],
-        }))
-      )
-    );
-  }
-
-  function handlOnClick() {
-    if (btnTextContent === "Sort Scores Descending") {
-      setBtnTextCont("Sort Scores Ascending");
-      sortScoresAscending();
-    } else {
-      sortScoresDescending();
-    }
-  }
+  };
 
   return (
     <>
       <h1>High Scores per Country</h1>
-      <button className="sort-btn" onClick={handlOnClick}>
-        {btnTextContent}
-      </button>
+      <SortButton setSortedScores={setSortedScores} />
       <HighScoreTable allCountryScores={sortedScores} />
     </>
   );
