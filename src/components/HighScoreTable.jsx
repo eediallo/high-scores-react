033081f38b1c788058e088339e2ff85b9cce5818
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-22 12:16:12 +0000

    refactor by removing first return

--- src/components/HighScoreTable.jsx
+++ src/components/HighScoreTable.jsx
@@ -1,15 +1,20 @@
 import PlayerCountry from "./PlayerCountry";
 import PlayersScores from "./PlayersScores";
+import PropTypes from "prop-types";
 
-const HighScoreTable = ({ allCountryScores }) => {
-  return allCountryScores.map((country) => {
-    return (
-      <div className="scores-container" key={country.id}>
-        <PlayerCountry id={country.id} name={country.name} />
-        <PlayersScores country={country} />
-      </div>
-    );
-  });
+export default function HighScoreTable({ allCountryScores }) {
+  return (
+    <>
+      {allCountryScores.map((country) => (
+        <div className="scores-container" key={country.id}>
+          <PlayerCountry id={country.id} {...country} />
+          <PlayersScores country={country} />
+        </div>
+      ))}
+    </>
+  );
+}
+
+HighScoreTable.propTypes = {
+  allCountryScores: PropTypes.array.isRequired,
 };
-
-export { HighScoreTable };
