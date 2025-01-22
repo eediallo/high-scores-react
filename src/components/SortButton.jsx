bfc20684fd8d49f55aa1cef80d6356e6fbb31663
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-22 13:27:07 +0000

    rename file and changes arrow func to func declaration for consistency

---
+++ src/components/SortButton.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import allCountryScores from "../../data/scores";
+import PropTypes from "prop-types";
+
+function SortCountryScoresByName(countries) {
+  return countries.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function SortScores(scores, ascending = true) {
+  return scores.sort((a, b) => (ascending ? b.s - a.s : a.s - b.s));
+}
+
+function SortButton({ setSortedCountryScores }) {
+  let [btnTextContent, setBtnTextCont] = useState("Sort Scores Descending");
+
+  function sortScoresAscending() {
+    setSortedCountryScores(
+      SortCountryScoresByName(
+        allCountryScores.map((country) => ({
+          ...country,
+          scores: [...SortScores(country.scores, false)],
+        }))
+      )
+    );
+  }
+
+  function sortScoresDescending() {
+    setBtnTextCont("Sort Scores Descending");
+    setSortedCountryScores(
+      SortCountryScoresByName(
+        allCountryScores.map((country) => ({
+          ...country,
+          scores: [...SortScores(country.scores)],
+        }))
+      )
+    );
+  }
+
+  function handleClick() {
+    if (btnTextContent === "Sort Scores Descending") {
+      setBtnTextCont("Sort Scores Ascending");
+      sortScoresAscending();
+    } else {
+      sortScoresDescending();
+    }
+  }
+
+  return (
+    <>
+      <button className="sort-btn" onClick={handleClick}>
+        {btnTextContent}
+      </button>
+    </>
+  );
+}
+
+SortButton.propTypes = {
+  setSortedCountryScores: PropTypes.func.isRequired,
+};
+
+export { SortButton, SortScores, SortCountryScoresByName };
