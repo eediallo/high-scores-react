0cfbb0a51364b0c646c4135fcdf7c1eb9623cb7a
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-22 12:06:12 +0000

    refactor -> creates PlayerScores component

---
+++ src/components/PlayerScores.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+export default function PlayerScores({ n, s }) {
+  return (
+    <tbody>
+      <tr className="score">
+        <td>{n.toUpperCase()}</td>
+        <td>
+          <span className="high-score">{s}</span>
+        </td>
+      </tr>
+    </tbody>
+  );
+}
+
+PlayerScores.propTypes = {
+  n: PropTypes.string.isRequired,
+  s: PropTypes.number.isRequired,
+};
